d76d4d0048055a0c3e8632c074331dd2aaeb53b8
iasyolp <iasyolp@gmail.com>
2017-05-01 00:03:55 +0700

    Initialize map screen

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -7,26 +7,49 @@ import {
   Dimensions
 } from 'react-native';
 
+// import map apis
 import MapView from 'react-native-maps';
 
+// dimension for map
 var {width, height} = Dimensions.get('window');
+
 
 export default class MapScreen extends Component {
 
-
+  // hide navigation bar
   static navigatorStyle = {
     navBarHidden: true
   };
 
   constructor(props) {
     super(props);
+    this.state ={
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    };
+    this.onRegionChange = this.onRegionChange.bind(this);
   }
 
+  onRegionChange(region) {
+    this.setState({region});
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.map}/>
+        <MapView style={styles.map}
+          mapType="standard"
+          showsUserLocation={true}
+          followsUserLocation={true}
+          showsCompass={true}
+          showsPointOfInterest={false}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        />
       </View>
     );
   }
