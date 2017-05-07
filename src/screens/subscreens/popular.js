bb18ab24cc9e930a63c2ad524387dc1bc9a5715e
Metis <dal2kh0und@gmail.com>
2017-05-07 16:03:40 +0700

    Revert "Test api key"
    This reverts commit d985c7c12d558eac0d9482cd4ee52d388843744c.

--- src/screens/subscreens/popular.js
+++ src/screens/subscreens/popular.js
@@ -54,7 +54,7 @@ class popular extends Component {
     this.state = {
       dataSource: ds.cloneWithRows([]),
     };
-    api.search('batman').then((data) => {
+    api.search('egg').then((data) => {
       this.setState({dataSource: ds.cloneWithRows(data)});
     });
     // this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
